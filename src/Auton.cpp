616efdcca592aca7dfc2a0630e4d52acc9326039
NicholasDip <npdipietropolo@gmail.com>
2026-01-14 18:43:41 -0500

    Stableized Automous

--- src/Auton.cpp
+++ src/Auton.cpp
@@ -19,20 +19,43 @@ rd::Selector selector({
 
 void autonomous() {
   //selector.run_auton();
-  odom.reset(-22.521, -22.462, 220);
-  
-  // Already at first point, start from second
-  move_to_point(-46.702, -47.182, 90, 30, 2500);
-  move_to_point(-25.358, -47.047, 90, 30, 2500);
+ odom.reset(0, 0, 0);
+   turn_to_angle(-26, 60, 1000);
+    intake_bottom.move_velocity(600);
+  intake_top.move_velocity(200);
+  intake_flex.move_velocity(200);
+  drive_straight(35, 35, 2000);
+ 
+  turn_to_angle(-140, 60, 2000);   // Turn to 90 degrees heading
+  drive_straight(37, 35, 2000);
+  turn_to_angle(-188, 60, 1000);
+  drive_straight(-36, 60, 1000);
+  piston.set_value(true);
+
 }
 
 void red_left_auton() {
-  // Test: forward 24", end facing 12°
-  move_to_point(24, 0, 12, 60, 2500);
-  move_to_point(15, 0, 0, 60, 2500);
+  // Simple drive forward test
+  odom.reset(0, 0, 0);
+  drive_straight(50, 60, 3000);  // Drive 50 inches forward at 60 speed
+  //turn_to_angle(90, 00, 2000);   // Turn to 90 degrees heading
 }
 
-void red_right_auton() {}
+void red_right_auton() {
+odom.reset(0, 0, 0);
+   turn_to_angle(-26, 60, 1000);
+    intake_bottom.move_velocity(600);
+  intake_top.move_velocity(200);
+  intake_flex.move_velocity(200);
+  drive_straight(35, 35, 2000);
+ 
+  turn_to_angle(-140, 60, 2000);   // Turn to 90 degrees heading
+  drive_straight(38, 35, 2000);
+  turn_to_angle(-186, 60, 1000);
+  drive_straight(-36, 35, 1000);
+  piston.set_value(true);
+
+}
 void blue_left_auton() {}
 void blue_right_auton() {}
 void skills_auton() {}
@@ -46,3 +69,11 @@ void skills_auton() {}
 //${code}
 //`
 //moveToPoint: `move_to_point(${x}, ${y}, ${heading}, ${speed}, 2500);`
+
+// Path
+
+//moveToPoint(22.115, -22.471, 265, 30);
+//moveToPoint(48.078, -46.162, 270, 30);
+//moveToPoint(29.465, -46.984, 270, 30);
+
+
