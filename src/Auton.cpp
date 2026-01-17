edd45b752466fb15c81ffea0989cb6e4d0784cf1
NicholasDip <npdipietropolo@gmail.com>
2026-01-17 08:15:42 -0500

    w

--- src/Auton.cpp
+++ src/Auton.cpp
@@ -20,16 +20,16 @@ rd::Selector selector({
 void autonomous() {
   //selector.run_auton();
  odom.reset(0, 0, 0);
-   turn_to_angle(-26, 60, 1000);
+   turn_to_angle(-24, 60, 1000);
     intake_bottom.move_velocity(600);
   intake_top.move_velocity(200);
   intake_flex.move_velocity(200);
   drive_straight(35, 35, 2000);
  
   turn_to_angle(-140, 60, 2000);   // Turn to 90 degrees heading
-  drive_straight(37, 35, 2000);
+  drive_straight(33.5, 35, 2000);
   turn_to_angle(-188, 60, 1000);
-  drive_straight(-36, 60, 1000);
+  drive_straight(-24, 60, 1000);
   piston.set_value(true);
 
 }
